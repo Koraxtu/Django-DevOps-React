8842891e78b5f8d7535872ef4986644f683ea780
Koraxtu <bukason2013@gmail.com>
2024-08-30 05:48:44 -0400

    Fixed test, added django backend and tests

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,23 @@
-from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin # type: ignore
+from django.db import models # type: ignore
+from django.utils import timezone # type: ignore
+from django.utils.translation import gettext_lazy as _ # type: ignore
 
-# Create your models here.
+from .managers import CustomUserManager
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(_('email address'), unique=True)
+    name = models.CharField(max_length=100)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+    sent_verification_email = models.BooleanField(default=False)
+    verified_email = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
--- frontend/src/tests/HappyPath.test.jsx
+++ frontend/src/tests/HappyPath.test.jsx
@@ -1,7 +1,12 @@
 import { screen, render } from "@testing-library/react";
+import { act } from "react";
 import userEvent from "@testing-library/user-event";
 import App from "../App";
 
 test("Happy path test", async () => {
-  render(<App />);
+  await act(async () => {
+    render(<App />);
+  });
+  const copyrightText = await screen.findAllByText(/copyright/i);
+  expect(copyrightText.length).toBeGreaterThan(0);
 });
